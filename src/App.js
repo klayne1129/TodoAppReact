3f3ff8fece178320fe121cfc8a49d09165b9bb12
Katie Layne <klayne1129@gmail.com>
2020-06-23 18:27:53 -0500

    added simple header

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends React.Component {
         
         return (
             <div className="todo-list">
+                <div>Let's Get Things Done!</div>
                 {todoItems}
             </div>
         )    
